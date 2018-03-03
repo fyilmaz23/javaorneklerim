3172b4c77133f42ad463885c0c3e020a06d96b06
vektorel <vektorel@lab11-14>
2018-03-03 15:53:08 +0300

    İlk eklemeler

---
+++ src/paket1/st_class.java
@@ -0,0 +1,52 @@
+package paket1;
+
+import java.util.Scanner;
+
+public class st_class {
+
+	private static Scanner input;
+
+	public static void main(String[] args) {
+
+		
+		hesapmakinesi();
+	}
+
+	private static void hesapmakinesi() {
+		input = new Scanner(System.in);
+		byte islem = 1;
+		System.out.println("1.Sayýyý Giriniz");
+		byte sayi1 = input.nextByte();
+
+		System.out.println("2.Sayýyý Giriniz");
+		byte sayi2 = input.nextByte();
+
+		System.out.println("1.Sayý= " + sayi1);
+		System.out.println("2.Sayý= " + sayi2);
+
+		System.out.println("\n\n\nToplama için 1 \nÇýkarma için 2 \nÇarpma Ýçin 3 \nBölme için 4 \n");
+		while (islem != 0) {
+			islem = input.nextByte();
+
+			if (islem == 1)
+				System.out.println("2 sayýnýn Toplamý: " + (sayi1 + sayi2));
+			else if (islem == 2)
+				System.out.println("2 Sayýnýn Farký: " + (sayi1 - sayi2));
+			else if (islem == 3)
+				System.out.println("2 Sayýnýn çarpýmý: " + (sayi1 * sayi2));
+			else if (islem == 4)
+				System.out.println("2 sayýnýn bölümü: " + (sayi1 / sayi2));
+			else if (islem == 0) {
+				System.out.println("Çýkýþ Yapýlýyor..");
+				break;
+			} else
+				System.out.println("Hatalý tuþlama yaptýnýz");
+			System.out.println("\n\nToplama için 1 \nÇýkarma için 2 \nÇarpma Ýçin 3 \nBölme için 4 \nÇýkýþ için 0\n");
+
+		}
+
+		System.out.println("Çýkýþ Baþarýyla Tamamlandý.");
+	
+	}
+
+}
---
+++ src/paket1/th_class.java
@@ -0,0 +1,13 @@
+package paket1;
+
+public class th_class {
+
+	
+	public static void main(String[] args) {
+		
+		int i=3;
+		System.out.println(i==3?"doðru":"yanlýþ");
+		System.out.println();
+	}
+
+}
